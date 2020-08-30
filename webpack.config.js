17364a2001b1a510cbb534b274d5ef290657c6b8
craigmellon <craigmellon@etech.net>
2020-08-30 18:28:31 +0000

    sass webpack config not-working

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ const shellConfig = {
   module: {
     rules: [
       { test: /\.ts$/, loader: '@ngtools/webpack' },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
     ],
   },
   plugins: [
@@ -29,7 +37,9 @@ const shellConfig = {
       directTemplateLoading: true,
       entryModule: path.resolve(__dirname, './projects/shell/src/app/app.module#AppModule'),
     }),
-    new CopyPlugin([{ from: 'projects/shell/src/assets', to: 'assets' }]),
+    new CopyPlugin({
+      patterns: [{ from: 'projects/shell/src/assets', to: 'assets' }],
+    }),
     new HtmlWebpackPlugin({
       template: './projects/shell/src/index.html',
     }),

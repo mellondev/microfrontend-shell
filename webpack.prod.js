631183c4442bf91d3251ce58d34bcd24db5bf402
Craig Mellon <craigmellon@etech.net>
2020-09-17 07:51:45 +0000

    fix public path

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,6 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    publicPath: 'https://red-wave-053790403.azurestaticapps.net/',
+    publicPath: 'https://blue-rock-08cf12003.azurestaticapps.net/',
   },
 });
